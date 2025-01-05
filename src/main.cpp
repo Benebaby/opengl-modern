16801735d199cf5787996d7da4f556c0d94b524b
Benedikt Krieger <benediktkrieger@outlook.com>
2025-01-05 10:35:04 +0100

    fix docking

--- src/main.cpp
+++ src/main.cpp
@@ -194,7 +194,8 @@ int main(int argc, char* argv[])
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
   io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-  //io.ConfigViewportsNoAutoMerge = true;
+  io.ConfigViewportsNoAutoMerge = true;
+  io.ConfigDockingTransparentPayload = true;
   //io.ConfigViewportsNoTaskBarIcon = true;
 
 
